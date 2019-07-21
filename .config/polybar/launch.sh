9ccc0433740d99914e6816fb3c77a16a99cbebb6
Zy1X_ <sbhk2020@gmail.com>
2019-07-21 16:53:22 +0530

    Remove execution of top polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar --config=$HOME/.config/polybar/config.ini top &
+#polybar --config=$HOME/.config/polybar/config.ini top &
 polybar --config=$HOME/.config/polybar/config.ini bottom &
 
 echo "Bars launched..."
